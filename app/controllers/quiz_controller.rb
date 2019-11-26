2e4078800af22b4d67e05667816abe71f146e680
dwarncke <daniel.warncke@gmail.com>
2019-11-26 14:44:13 +0000

    initial point system

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,4 +4,35 @@ class QuizController < ApplicationController
 
   def new
   end
+
+  def create
+    character_counts = {
+      "Jon Snow" => 0,
+      "Cersei Lannister" => 0,
+      "Khaleesi" => 0,
+      "Arya Stark" => 0
+      "Sansa Stark" => 0
+      "Tyrion Lannister" => 0
+      "Margaery Tyrell" => 0
+
+    }
+    quiz_params.values.each do |answer| 
+      QuizHelper.preferences.each do |name, preference|
+          if preference.include? answer
+            p character_counts[name] += 1
+          end
+      end
+    end
+    p character_counts
+    
+  end
+
+  private
+
+  def quiz_params
+    params.require(:got).permit(:question1, :question2, :question3, :question4, :question5, :question6, :question7, :question8)
+  end
 end
+
+
+
--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,2 +1,11 @@
 module QuizHelper
+  def self.preferences
+     {
+      "Jon Snow" => ["hiphop", "wolf"],
+      "Cersei Lannister" => ["classical", "lion"],
+      "Khaleesi" => ["indiepop", "dragon"],
+      "Bronn" => ["rocknroll", "giraffe"]
+    }
+
+  end
 end
