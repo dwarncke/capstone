ee3e43f1b0cf511e3bd2a284f1124bf6243a1dfb
dwarncke <daniel.warncke@gmail.com>
2019-11-27 22:24:05 +0000

    redirect_to

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -24,17 +24,36 @@ class QuizController < ApplicationController
           end
       end
     end
-    p character_counts
-      
     
+    
+
+    p character_counts
+     if character_counts = "David Rose"
+      redirect_to david_path
+    end
   end
+    
+  
+    
+  
+    
+  
 
   private
+
 
   def quiz_params
     params.require(:schitt).permit(:question1, :question2, :question3, :question4, :question5, :question6, :question7, :question8)
   end
 end
+
+
+
+
+
+
+
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :quiz
   get 'new_quiz', to: 'quiz#new'
   get 'quiz_index', to: 'quiz#index'
+  get 'david', to: 'quiz#david'
+  get 'alexis', to: 'quiz#alexis'
+  get 'stevie', to: 'quiz#stevie'
+  get 'patrick', to: 'quiz#patrick'
+  get 'ted', to: 'quiz#ted'
+  get 'johnny', to: 'quiz#johnny'
+  get 'moira', to: 'quiz#moira'
+  get 'rowland', to: 'quiz#rowland'
 end
