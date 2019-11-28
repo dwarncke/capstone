7067bb840b725885034d5775b28d407e4d124618
dwarncke <daniel.warncke@gmail.com>
2019-11-28 21:34:18 +0000

    updated route process

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -5,39 +5,51 @@ class QuizController < ApplicationController
   def new
   end
 
+  def show
+    @name = params[:character]
+
+
+  end
+
   def create
     character_counts = {
-      "David Rose" => 0,
-      "Alexis Rose" => 0,
-      "Stevie Budd" => 0,
+      "DavidRose" => 0,
+      "AlexisRose" => 0,
+      "StevieBudd" => 0,
       "Patrick" => 0,
-      "Ted Mullens" => 0,
-      "Johnny Rose" => 0,
-      "Moira Rose" => 0,
-      "Rowland Schitt" => 0
+      "TedMullens" => 0,
+      "JohnnyRose" => 0,
+      "MoiraRose" => 0,
+      "RowlandSchitt" => 0
     }
 
     quiz_params.values.each do |answer| 
       QuizHelper.preferences.each do |name, preference|
           if preference.include? answer
-            p character_counts[name] += 1
+            character_counts[name] += 1
           end
       end
     end
     
-    
+    max_value = 0
+    chosen_name = ""
 
-    p character_counts
-     if character_counts = "David Rose"
-      redirect_to david_path
+    character_counts.each do |name, count|
+      if count > max_value
+        max_value = count
+        p chosen_name = name
+      end
     end
+
+
+
+     redirect_to results_path({character: chosen_name})
+
+
+
+     
+    
   end
-    
-  
-    
-  
-    
-  
 
   private
 
@@ -46,6 +58,9 @@ class QuizController < ApplicationController
     params.require(:schitt).permit(:question1, :question2, :question3, :question4, :question5, :question6, :question7, :question8)
   end
 end
+
+
+
 
 
 
--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,14 +1,14 @@
 module QuizHelper
   def self.preferences
      {
-      "David Rose" => ["nolabel", "simple", "drama", "edgy", "noshow", "europe", "curator"],
-      "Alexis Rose" => ["extrovert", "activity", "sandiego", "romcom", "expensive", "chat", "curator"],
-      "Stevie Budd" => ["nolabel", "dont", "colorado", "action", "sophisticated", "airbnb", "actor"],
+      "DavidRose" => ["nolabel", "simple", "drama", "edgy", "noshow", "europe", "curator"],
+      "AlexisRose" => ["extrovert", "activity", "sandiego", "romcom", "expensive", "chat", "curator"],
+      "StevieBudd" => ["nolabel", "dont", "colorado", "action", "sophisticated", "airbnb", "actor"],
       "Patrick" => ["introvert", "simple", "asheville", "action", "food", "camping", "cafeowner"],
-      "Ted Mullens" => ["extrovert", "adventure", "asheville", "western", "casual", "drunk", "europe"],
-      "Johnny Rose" => ["activity", "newyorkcity", "romcom", "sophisticated", "noshow", "roadtrip", "lawyer"],
-      "Moira Rose" => ["between", "newyorkcity", "drama", "expensive", "drunk", "airbnb", "actor"],
-      "Rowland Schitt" => ["between", "adventure", "colorado", "casual", "chat", "camping", "cafeowner"]
+      "TedMullens" => ["extrovert", "adventure", "asheville", "western", "casual", "drunk", "europe"],
+      "JohnnyRose" => ["activity", "newyorkcity", "romcom", "sophisticated", "noshow", "roadtrip", "lawyer"],
+      "MoiraRose" => ["between", "newyorkcity", "drama", "expensive", "drunk", "airbnb", "actor"],
+      "RowlandSchitt" => ["between", "adventure", "colorado", "casual", "chat", "camping", "cafeowner"]
     }
 
   end
